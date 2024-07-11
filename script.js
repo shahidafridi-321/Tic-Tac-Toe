9872efa513ef3e22fc44f4ae2a74f2e6efbc4386
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-07-11 13:13:34 +0500

    dom creation

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-let Gameboard = (
-  function () {
-    let gameBoard = new Array(9).fill('');
+// Gameboard Module
+let Gameboard = (function () {
+  let gameBoard = new Array(9).fill('');
 
-    let getBoard = () => gameBoard;
+  let getBoard = () => gameBoard;
 
-    let resetBoard = () => {
-      for (let i = 0; i < gameBoard.length; i++) {
-        gameBoard[i] = '';
-      }
-    };
+  let resetBoard = () => {
+    gameBoard.fill('');
+  };
 
-    let placeMark = (cell, mark) => {
-      if (gameBoard[cell] === '') {
-        gameBoard[cell] = mark;
-        return true
-      }
-      return false
-    };
-
-    return {
-      getBoard,
-      resetBoard,
-      placeMark,
+  let placeMark = (cell, mark) => {
+    if (gameBoard[cell] === '') {
+      gameBoard[cell] = mark;
+      return true;
     }
-  }
-)();
+    return false;
+  };
 
+  return {
+    getBoard,
+    resetBoard,
+    placeMark,
+  };
+})();
+
+// Player Factory
 let Players = function (name, symbol) {
   return {
     name,
-    symbol
-  }
+    symbol,
+  };
 };
 
-let GameController = function () {
+// GameController Module
+let GameController = (function () {
   let playerOne = Players('shahid', 'X');
   let playerTwo = Players('Khan', 'O');
   let currentPlayer = playerOne;
@@ -43,7 +42,7 @@ let GameController = function () {
     Gameboard.resetBoard();
     currentPlayer = playerOne;
     gameStatus = 'active';
-  }
+  };
 
   let switchTurn = () => {
     currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
@@ -56,6 +55,7 @@ let GameController = function () {
         console.log(`${currentPlayer.name} has won`);
       } else if (checkDraw()) {
         gameStatus = 'draw';
+        console.log('The game is a draw');
       } else {
         switchTurn();
       }
@@ -72,7 +72,7 @@ let GameController = function () {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6]
+      [2, 4, 6],
     ];
 
     for (let combination of winningCombinations) {
@@ -86,34 +86,77 @@ let GameController = function () {
 
   let checkDraw = () => {
     let board = Gameboard.getBoard();
-    return board.every(cell => cell !== '')
+    return board.every((cell) => cell !== '');
   };
 
   return {
     startGame,
-    switchTurn,
     makeMove,
-    checkWin,
-    checkDraw,
     getCurrentPlayer: () => currentPlayer,
     getStatus: () => gameStatus,
-  }
-};
+  };
+})();
 
-// Create a single instance of GameController
-let round1 = GameController();
+// DOM Manipulation Module
+let DOM = (function () {
+  // Selecting buttons and other necessary elements
+  let btns = document.querySelectorAll('.gameboard-container button');
+  let startBtn = document.querySelector('.startBtn');
+  let restartBtn = document.querySelector('.restartBtn');
 
-// Start the game
-round1.startGame();
+  // Create a single instance of domActions
+  let actions = domActions();
 
-// Make a move and check the board
-round1.makeMove(5);
-round1.makeMove(0);
-round1.makeMove(6);
-round1.makeMove(1);
-round1.makeMove(7);
-round1.makeMove(2);
-console.log();
-console.log(Gameboard.getBoard());
-console.log(round1.getCurrentPlayer());
+  // Add event listeners to the game board buttons
+  btns.forEach((btn, index) => {
+    btn.addEventListener('click', (event) => {
+      actions.playRound(event, index);
+    });
+  });
 
+  // Add event listeners to the start and restart buttons
+  startBtn.addEventListener('click', actions.startGame);
+  restartBtn.addEventListener('click', actions.startGame);
+
+  // Function to update the game board UI
+  let updateBoard = () => {
+    let board = Gameboard.getBoard();
+    btns.forEach((btn, index) => {
+      btn.textContent = board[index];
+    });
+  };
+
+  // Return updateBoard so it can be used in domActions
+  return {
+    updateBoard,
+  };
+})();
+
+// Factory function for DOM actions
+function domActions() {
+  // Create an instance of the game controller
+  let round = GameController;
+
+  // Function to start the game
+  let startGame = () => {
+    round.startGame();
+    DOM.updateBoard();
+  };
+
+  // Function to handle a player's move
+  let playRound = (event, index) => {
+    round.makeMove(index);
+    DOM.updateBoard();
+  };
+
+  // Return the functions to be used as event handlers
+  return {
+    startGame,
+    playRound,
+  };
+}
+
+// Initialize the game by calling startGame once
+document.addEventListener('DOMContentLoaded', () => {
+  domActions().startGame();
+});
